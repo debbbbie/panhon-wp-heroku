06f9dada938fba43b77346b11c7542120af9bcbe
debbbbie <debbbbie@163.com>
2017-01-08 21:02:21 +0800

    footer

--- wp-content/themes/lawyeria-lite/footer.php
+++ wp-content/themes/lawyeria-lite/footer.php
@@ -12,11 +12,11 @@
 						if ( is_active_sidebar( 'footer-sidebar' ) ) {
 							dynamic_sidebar( 'footer-sidebar' );
 						} else {
-							echo '<div class="footer-box">'.__('磐宏中国（上海）<br/>地址：上海市闵行区江川路1777号<br/>电话：021-64161712').'</div>';
+							echo '<div class="footer-box">'.__('磐宏中国（上海）<br/>地址：上海市闵行区江川路1777号<br/>电话：021-64161712','lawyeria-lite').'</div>';
 						}
 					?>
 				</div><!--/div .footer-margin-left .cf-->
-				<?php echo '<div class="lawyeria_lite_poweredby"><a class="" href="https://themeisle.com/themes/lawyeria-lite/" target="_blank" rel="nofollow">Lawyeria Lite </a>'.__('powered by','lawyeria-lite').'<a class="" href="http://wordpress.org/" target="_blank" rel="nofollow"> WordPress</a></div>'; ?>
+				<!--<-?-php echo '<div class="lawyeria_lite_poweredby"><a class="" href="https://themeisle.com/themes/lawyeria-lite/" target="_blank" rel="nofollow">Lawyeria Lite </a>'.__('powered by','lawyeria-lite').'<a class="" href="http://wordpress.org/" target="_blank" rel="nofollow"> WordPress</a></div>'; ?>-->
 			</div><!--/div .wrapper .cf-->
 		</footer><!--/footer #footer-->
 		<?php wp_footer(); ?>
