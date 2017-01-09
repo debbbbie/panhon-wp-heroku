c805d1e0f4d8bd53a02c6fcd56a68b4842eba229
debbbbie <debbbbie@163.com>
2017-01-09 09:15:02 +0800

    favicon.ico

--- wp-content/themes/lawyeria-lite/header.php
+++ wp-content/themes/lawyeria-lite/header.php
@@ -14,9 +14,9 @@
 		<title><?php wp_title('|', true, 'right'); ?></title>
 		<link rel="icon" href="/favicon.ico" type="image/x-icon"/>
      <meta content="debbbbie" name="author" />
-		 <link href="http://www.jinshisong.cn/icon/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
-		 <link href="http://www.jinshisong.cn/icon/favicon.ico" rel="apple-touch-icon" sizes="64x64" type="image/png" />
-		 <link href="http://www.jinshisong.cn/icon/favicon.ico" rel="icon" sizes="64x64" type="image/png" />
+		 <link href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+		 <link href="/favicon.ico" rel="apple-touch-icon" sizes="64x64" type="image/png" />
+		 <link href="/favicon.ico" rel="icon" sizes="64x64" type="image/png" />
 
 		<!--[if lt IE 9]>
 		<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
